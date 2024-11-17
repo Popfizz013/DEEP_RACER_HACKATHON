32586d17f7bddd1b2b54625faf9fe7d9e83539e5
Mikayla <mikayla@Mikaylas-MacBook-Air.local>
2024-11-16 17:23:47 -0800

    v2 of stinkMobile implementing wide turn, comments added

--- stinkMobile.py
+++ stinkMobile.py
@@ -9,21 +9,32 @@ def reward_function(params):
     reward = 1e-3 
 
     # Read input parameters
-    track_width = params['track_width']
-    distance_from_center = params['distance_from_center']
-    left = params["is_left_of_center"]
-    on_track= params["all_wheels_on_track"]
+    track_width: float = params['track_width']
+    distance_from_center: float = params['distance_from_center']
+    left: bool = params["is_left_of_center"]
+    on_track: bool= params["all_wheels_on_track"]
+
+    # Reports x-coordinate 
+    cur_heading: float = params["heading"]
+
+    # Flag to indicate if vehicle is on straight away
+    on_straight: bool = False
+
+    # Sets flag if vehicle is on straight away
+    if ((-10 <= cur_heading) and (cur_heading <= 10)):
+        on_straight = True
 
     # Calculate 3 markers that are at varying distances away from the center line
     quad_1 = 0.25 * track_width
     quad_2 = 0.50 * track_width
     quad_3 = 0.75 * track_width
     
-    if left:
+    # Set reward zones to favour wide turns after straight away or tight corners in sequence
+    if on_straight or left:
         if (distance_from_center <= quad_2 and distance_from_center>= quad_1):
-            reward=1
+            reward = 1
         elif (distance_from_center <= quad_3 and on_track):
-            reward=0.5 
+            reward = 0.5 
     else:
         # Give higher reward if the car is closer to center line and vice versa
         if distance_from_center <= quad_1:

1d666f1fc336952d28fd62e858d4a29fee2931a9
Liam Degand <degandliam013@gmail.com>
2024-11-18 22:05:47 -0800

    final version for competition
    Made some adjustment and chose to train V2 some more with the things we learnt from training V3

--- stinkMobile.py
+++ stinkMobile.py
@@ -4,10 +4,6 @@ def reward_function(params):
     '''
     Example of rewarding the agent to follow center line
     '''
-
-    # Initialize reward as the "penalty"
-    reward = 1e-3 
-
     # Read input parameters
     track_width: float = params['track_width']
     distance_from_center: float = params['distance_from_center']
@@ -19,29 +15,44 @@ def reward_function(params):
     cur_heading: float = params["heading"]
 
     # Flag to indicate if vehicle is on straight away
-    on_straight: bool = True
+    on_straight: bool = False
 
     # Sets flag if vehicle is on straight away
     if ((-10 <= cur_heading) and (cur_heading <= 10)):
-        on_straight = False
+        on_straight = True
 
     # Calculate 3 markers that are at varying distances away from the center line
     quad_1 = 0.25 * track_width
     quad_2 = 0.50 * track_width
     quad_3 = 0.75 * track_width
     
+    # Initialize reward
+    reward=0
     # Set reward zones to favour wide turns after straight away or tight corners in sequence
-    if ((on_straight and not left) or (left and not on_straight)):      
-        if (distance_from_center <= quad_2 and distance_from_center >= quad_1):
+    if on_straight:
+        if left:
+            if (distance_from_center <= quad_2 and distance_from_center>= quad_1):
+                reward = 0.8
+            elif (distance_from_center <= quad_3 and on_track):
+                reward = 0.4 
+        else:
+            if (distance_from_center <= quad_2 and distance_from_center>= quad_1):
+                reward = 1
+            elif (distance_from_center <= quad_3 and on_track):
+                reward = 0.5 
+    elif left:
+        if (distance_from_center <= quad_2 and distance_from_center>= quad_1):
             reward = 1
         elif (distance_from_center <= quad_3 and on_track):
-            reward = 0.5
+            reward = 0.5 
     else:
         # Give higher reward if the car is closer to center line and vice versa
         if distance_from_center <= quad_1:
-            reward = 0.25
+            reward = 0.5
         elif distance_from_center <= quad_2:
             reward = 0.1
+        else:
+            reward = 1e-3
 
 
     ABS_STEERING_THRESHOLD = 15
